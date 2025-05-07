b0da76948397e820b8c8ecbb4ca0596e20427400
Gaeon <gaeon0413@gmail.com>
2025-05-07 19:20:39 +0900

    구현체가 아닌 인터페이스 주입으로 수정

--- backend/src/main/java/com/askmentor/controller/UserController.java
+++ backend/src/main/java/com/askmentor/controller/UserController.java
@@ -3,28 +3,28 @@ package com.askmentor.controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import com.askmentor.model.User;
-import com.askmentor.service.UserServiceImpl;
+import com.askmentor.service.UserService;
 import com.askmentor.dto.UserUpdateRequest;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
-	private final UserServiceImpl UserServiceImpl;
+	private final UserService userService;
 
-	public UserController(UserServiceImpl UserServiceImpl) {
-		this.UserServiceImpl = UserServiceImpl;
+	public UserController(UserService userService) {
+		this.userService = userService;
 	}
 
 	@GetMapping("/{user_id}")
 	public ResponseEntity<User> getUser(@PathVariable int user_id) {
-		User user = UserServiceImpl.getUser(user_id);
+		User user = userService.getUser(user_id);
 		return ResponseEntity.ok(user);
 	}
 
 	@PatchMapping("/{user_id}")
 	public ResponseEntity<String> updateUser(@PathVariable int user_id, @RequestBody UserUpdateRequest request) {
-		String result = UserServiceImpl.updateUser(user_id, request);
+		String result = userService.updateUser(user_id, request);
 		return ResponseEntity.ok(result);
 	}
 }
