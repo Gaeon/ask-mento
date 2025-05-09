591193ece055aeb342573c5806d87e18d1967400
hgit600 <hunique8832@naver.com>
2025-05-09 17:30:13 +0900

    sql문 1차 수정

--- db-init/init.sql
+++ db-init/init.sql
@@ -49,7 +49,7 @@ CREATE TABLE IF NOT EXISTS answers (
   question_id   INT NOT NULL,
   user_id       INT NOT NULL,
   answer_text   TEXT,
-  timestamp     TIMESTAMP,
+  timestamp     TIMESTAMP NULL DEFAULT NULL,
   satisfaction  INT,
   PRIMARY KEY (answer_id),
   KEY idx_answer_question (question_id),
